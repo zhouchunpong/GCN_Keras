4dcb63e82c4ddea808e06ff228b97e63c7573782
zhoudaxia <zhouchunpong@163.com>
2019-07-19 20:18:39 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+from keras.layers import Input, Dropout
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.utils import plot_model
+from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
+
+
+from graph import GraphConvolution
+from utils import *
+
+
+# Define parameters
+FILTER = 'localpool'  # 'localpool' or 'chebyshev'
+MAX_DEGREE = 2  # maximum polynomial degree
+SYM_NORM = True  # symmetric (True) vs. left-only (False) normalization
+NB_EPOCH = 200
+PATIENCE = 10  # early stopping patience
+
+
+# Get data
+# X:features  A:graph  y:labels
+X, A, y = load_data(dataset='cora', use_feature=True)
+y_train, y_val, y_test, train_mask, val_mask, test_mask = get_splits(y)
+
+# Normalize X
+X /= X.sum(1).reshape(-1, 1)
+
+
+if FILTER == 'localpool':
+    """ Local pooling filters (see 'renormalization trick' in Kipf & Welling, arXiv 2016) """
+    print('Using local pooling filters...')
+    A_ = preprocess_adj(A, SYM_NORM)
+    support = 1
+    graph = [X, A_]
+    G = [Input(shape=(None, None), batch_shape=(None, None), sparse=True)]
+
+elif FILTER == 'chebyshev':
+    """ Chebyshev polynomial basis filters (Defferard et al., NIPS 2016)  """
+    print('Using Chebyshev polynomial basis filters...')
+    L = normalized_laplacian(A, SYM_NORM)
+    L_scaled = rescale_laplacian(L)
+    T_k = chebyshev_polynomial(L_scaled, MAX_DEGREE)
+    support = MAX_DEGREE + 1
+    graph = [X]+T_k
+    G = [Input(shape=(None, None), batch_shape=(None, None), sparse=True)
+         for _ in range(support)]
+
+else:
+    raise Exception('Invalid filter type.')
+
+X_in = Input(shape=(X.shape[1],))
+
+
+# Define model architecture
+# NOTE: We pass arguments for graph convolutional layers as a list of tensors.
+H = Dropout(rate=0.5)(X_in)
+H = GraphConvolution(16, support, activation='relu',
+                     kernel_regularizer=l2(5e-4))([H]+G)
+H = Dropout(rate=0.5)(H)
+Y = GraphConvolution(y.shape[1], support, activation='softmax')([H]+G)
+
+# Compile model
+model = Model(inputs=[X_in]+G, outputs=Y)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(lr=0.01), weighted_metrics=['acc'])
+#model.summary()
+
+
+
+# Callbacks for EarlyStopping
+es_callback = EarlyStopping(monitor='val_weighted_acc', patience=PATIENCE)
+
+# Train
+validation_data = (graph, y_val, val_mask)
+model.fit(graph, y_train, sample_weight=train_mask,
+          batch_size=A.shape[0], 
+          epochs=NB_EPOCH,
+          verbose=1,
+          validation_data=validation_data,
+          shuffle=False,
+          callbacks=[es_callback])
+
+# Evaluate model on the test data
+eval_results = model.evaluate(graph,y_test,
+                              sample_weight=test_mask,
+                              batch_size=A.shape[0])
+print('Test Done.\n'
+      'Test loss: {}\n'
+      'Test accuracy: {}'.format(*eval_results))
---
+++ utils.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+
+import scipy.sparse as sp
+import numpy as np
+import networkx as nx
+from sklearn import preprocessing
+from keras.utils import to_categorical
+from scipy.sparse.linalg.eigen.arpack import eigsh, ArpackNoConvergence
+
+
+def encode_onehot(labels):
+    classes = set(labels)
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+def load_data(path="data/cora/", dataset="cora",use_feature=True):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(path))
+
+    idx_features_labels=np.loadtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+
+    labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    g=nx.read_edgelist("{}{}.cites".format(path, dataset))
+    N=len(g)
+    adj=nx.to_numpy_array(g,nodelist=idx_features_labels[:, 0])
+    adj = sp.coo_matrix(adj)
+
+    if use_feature:
+        features = np.array(idx_features_labels[:, 1:-1], dtype=np.float32)
+    else:
+        features=np.identity(N,dtype=np.float32)
+    
+    print('Dataset has {} nodes, {} edges, {} features.'.format(adj.shape[0], g.size(), features.shape[1]))
+
+    return features, adj, labels
+
+
+def normalize_adj(adj, symmetric=True):
+    if symmetric:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -0.5).flatten(), 0)
+        a_norm = adj.dot(d).transpose().dot(d).tocsr()
+    else:
+        d = sp.diags(np.power(np.array(adj.sum(1)), -1).flatten(), 0)
+        a_norm = d.dot(adj).tocsr()
+    return a_norm
+
+
+def preprocess_adj(adj, symmetric=True):
+    adj = adj + sp.eye(adj.shape[0])
+    adj = normalize_adj(adj, symmetric)
+    return adj
+
+
+def sample_mask(idx, l):
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
+
+
+def get_splits(y):
+    idx_train = range(140)
+    idx_val = range(200, 500)
+    idx_test = range(500, 1500)
+    
+    y_train = np.zeros(y.shape, dtype=np.int32) #y:label
+    y_val = np.zeros(y.shape, dtype=np.int32)
+    y_test = np.zeros(y.shape, dtype=np.int32)
+
+    y_train[idx_train] = y[idx_train]
+    y_val[idx_val] = y[idx_val]
+    y_test[idx_test] = y[idx_test]
+
+    train_mask = sample_mask(idx_train, y.shape[0])
+    val_mask = sample_mask(idx_val, y.shape[0])
+    test_mask = sample_mask(idx_test, y.shape[0])
+
+    return y_train, y_val, y_test, train_mask,val_mask,test_mask
+
+
+def normalized_laplacian(adj, symmetric=True):
+    adj_normalized = normalize_adj(adj, symmetric)
+    laplacian = sp.eye(adj.shape[0]) - adj_normalized
+    return laplacian
+
+
+def rescale_laplacian(laplacian):
+    try:
+        print('Calculating largest eigenvalue of normalized graph Laplacian...')
+        largest_eigval = eigsh(laplacian, 1, which='LM', return_eigenvectors=False)[0]
+    except ArpackNoConvergence:
+        print('Eigenvalue calculation did not converge! Using largest_eigval=2 instead.')
+        largest_eigval = 2
+
+    scaled_laplacian = (2. / largest_eigval) * laplacian - sp.eye(laplacian.shape[0])
+    return scaled_laplacian
+
+
+def chebyshev_polynomial(X, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    T_k = list()
+    T_k.append(sp.eye(X.shape[0]).tocsr())
+    T_k.append(X)
+
+    def chebyshev_recurrence(T_k_minus_one, T_k_minus_two, X):
+        X_ = sp.csr_matrix(X, copy=True)
+        return 2 * X_.dot(T_k_minus_one) - T_k_minus_two
+
+    for i in range(2, k+1):
+        T_k.append(chebyshev_recurrence(T_k[-1], T_k[-2], X))
+
+    return T_k
