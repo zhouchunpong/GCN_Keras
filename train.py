2fac5ed8681c1a73065b147f7350c3b9f471e6d8
zhoudaxia <zhouchunpong@163.com>
2020-01-24 21:57:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ FILTER = 'localpool'  # 'localpool' or 'chebyshev'
 MAX_DEGREE = 2  # maximum polynomial degree
 SYM_NORM = True  # symmetric (True) vs. left-only (False) normalization
 NB_EPOCH = 200
-PATIENCE = 10  # early stopping patience
+PATIENCE = 30  # early stopping patience
 
 
 # Get data
